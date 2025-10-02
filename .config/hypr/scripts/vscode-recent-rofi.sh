e058929e09e329603e4e31af9ba767b7dc001688
Fabio Rosada <fabio.rosada@greendecision.eu>
2025-10-02 18:52:12 +0200

    updated rofi theme, special theme for vscode dmenu

--- .config/hypr/scripts/vscode-recent-rofi.sh
+++ .config/hypr/scripts/vscode-recent-rofi.sh
@@ -2,7 +2,7 @@ recent=$(jq -r '.profileAssociations.workspaces' ~/.config/Code/User/globalStora
 recent=$(echo "$recent" | grep -o '"file://.*"' | sed 's|^"file://||; s|"$||')
 recent=$(echo "$recent" | tac)
 
-selected=$(echo "$recent" | rofi -theme ~/.config/rofi/launchers/type-1/style-8 -lines 7 -dmenu -i -p 'a' \
+selected=$(echo "$recent" | rofi -theme ~/.config/rofi/launchers/type-1/style-8-vscode -lines 7 -dmenu -i -p 'a' \
 		-columns 1)
 
 if [ -z "$selected" ]; then
