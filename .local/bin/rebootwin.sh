a78fa1d4895f37525ccf2d3c32b581e8d9059869
Fabio Rosada <r.fabio95@gmail.com>
2025-12-06 14:37:55 +0100

    fixed rebootwin for wlogout

---
+++ .local/bin/rebootwin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "################"
+echo "# REBOOTING TO #"
+echo "#   WINDOWS   #"
+echo "################"
+echo ""
+echo "To boot into windows directly, a password is required."
+echo ""
+
+sudo bootnext windows now
