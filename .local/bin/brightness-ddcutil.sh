0ed530d75e97bf513a4d2e50a5cdef146c3db3a4
Fabio Rosada <r.fabio95@gmail.com>
2026-06-10 12:01:13 +0200

    added display detection

--- .local/bin/brightness-ddcutil.sh
+++ .local/bin/brightness-ddcutil.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+# Detect available DDC displays
+DISPLAYS=$(ddcutil detect | awk '/Display [0-9]+/ {gsub(":","",$2); print $2}')
+
+# Exit silently if no monitors support DDC/CI
+if [ -z "$DISPLAYS" ]; then
+  exit 0
+fi
+
 if [ -z "$1" ]; then
-  CURRENT=$(ddcutil --display 1 getvcp 10 | sed -n 's/.*current value = *\([0-9]\+\).*/\1/p')
+  CURRENT=$(ddcutil --display 1 getvcp 10 2>/dev/null |
+    sed -n 's/.*current value = *\([0-9]\+\).*/\1/p')
 
   if [ "$CURRENT" -eq 100 ]; then
     LEVEL=50
   else
     LEVEL=100
   fi
-
 else
   LEVEL="$1"
 fi
 
-for d in 2 1; do
+# Apply to all detected displays
+for d in $DISPLAYS; do
   ddcutil --display "$d" setvcp 10 "$LEVEL"
 done
 
