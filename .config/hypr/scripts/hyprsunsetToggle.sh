62eb6613a24f220dc279f12a6b6fd3a1f85f922f
Fabio Rosada <r.fabio95@gmail.com>
2026-07-29 14:56:39 +0200

    New waybar music module

--- .config/hypr/scripts/hyprsunsetToggle.sh
+++ .config/hypr/scripts/hyprsunsetToggle.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# check if hyprsunset is running
+hyprsunset_is_on() {
+    local temp
+    temp=$(hyprctl hyprsunset temperature)
+    if [ -z "$temp" ] || [ "$temp" -eq "6000" ]; then
+        return 1
+    fi
+    return 0
+}
+
+
+
 
 # if --get or -g is passed as an argument, just return if hyprsunset is on or off
-
 if [ "$1" == "--get" ] || [ "$1" == "-g" ]
 then
-    if pgrep -x "hyprsunset" > /dev/null
+    if hyprsunset_is_on
     then
         echo "{\"text\":\"\",\"tooltip\":\"Click to turn hypersunset off\",\"class\":\"on\",\"alt\":\"on\"}"
     else
@@ -15,26 +24,25 @@ then
     exit 0
 fi
 
-if [ "$1" == "off" ]
-then
-    hyprctl hyprsunset identity &
-    # return a json object to be used in waybar
-    # echo "{\"text\":\"\",\"tooltip\":\"Hyprsunset is off\",\"class\":\"off\"}"
-    exit 0
-else
-    # turn on hyprsunset with the given temperature or default to 4000k
-    hyprctl hyprsunset temperature ${1:-4000k} &
-    # return a json object to be used in waybar
-    # echo "{\"text\":\"\",\"tooltip\":\"Hyprsunset is on\",\"class\":\"on\"}"
-    exit 0
-fi
+#if [ "$1" == "off" ]
+#then
+#    hyprctl hyprsunset identity &
+#    # return a json object to be used in waybar
+#    # echo "{\"text\":\"\",\"tooltip\":\"Hyprsunset is off\",\"class\":\"off\"}"
+#    exit 0
+#else
+#    # turn on hyprsunset with the given temperature or default to 4000k
+#    hyprctl hyprsunset temperature ${1:-4000k} &
+#    # return a json object to be used in waybar
+#    # echo "{\"text\":\"\",\"tooltip\":\"Hyprsunset is on\",\"class\":\"on\"}"
+#    exit 0
+#fi
 
-# OLD VERSION
-# better because it checked the current state, but right now it is broken
-if pgrep -x "hyprsunset" > /dev/null
+
+if hyprsunset_is_on
 then
     # echo "{\"text\":\"\",\"tooltip\":\"Hyprsunset is off\",\"class\":\"off\"}"
-    hyprctl hyprsunset identity &
+    hyprctl hyprsunset temperature 6000 &
     # return a json object to be used in waybar
 else
     hyprctl hyprsunset temperature ${1:-4000k} &
--- .config/waybar/scripts/playerctlModule.sh
+++ .config/waybar/scripts/playerctlModule.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-if [[ "$1" == "-t" ]]; then
+if [[ "$1" == "-i" ]]; then
   if playerctl status &> /dev/null; then
-    echo "true"
+    url=$(playerctl metadata xesam:url 2> /dev/null || echo "")
+    if [[ "$url" == *youtube* ]]; then
+      echo "youtube music playing"
+      exit 1
+    else
+      echo "real music playing"
+      exit 0
+    fi
   else
-    echo "false"
+    exit 1
   fi
 fi
 
-if [[ "$1" == "-a" ]]; then
+if [[ "$1" == "-p" ]]; then
   if playerctl status &> /dev/null; then
     status=$(playerctl status)
     if [[ "$status" == "Playing" ]]; then
@@ -16,8 +23,22 @@ if [[ "$1" == "-a" ]]; then
     else
       icon=""
     fi
-    echo "$icon $(playerctl metadata title) - $(playerctl metadata artist)"
+    echo "{\"text\":\"$icon $(playerctl metadata title)\", \"tooltip\":\"$(playerctl metadata artist) - $(playerctl metadata album) ($(playerctl metadata xesam:contentCreated))\", \"class\":\"$status\"}"
   else
-    echo "No music playing"
+    exit 0
+  fi
+fi
+
+if [[ "$1" == "-t" ]]; then
+  if playerctl status &> /dev/null; then
+    status=$(playerctl status)
+    if [[ "$status" == "Playing" ]]; then
+      icon=""
+    else
+      icon=""
+    fi
+    echo "{\"text\":\"$icon $(playerctl metadata title) - $(playerctl metadata artist)\", \"tooltip\":\"Click to toggle play/pause\", \"class\":\"$status\"}"
+  else
+    exit 0
   fi
 fi
