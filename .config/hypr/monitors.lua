8e58f210273b834ef91a5e398a5ab7e890178ab3
Fabio Rosada <r.fabio95@gmail.com>
2026-08-19 13:52:12 +0200

    added secondary monitor for desktop pc

--- .config/hypr/monitors.lua
+++ .config/hypr/monitors.lua
@@ -13,12 +13,17 @@ hl.monitor({
 })
 
 hl.monitor({
+  output = "HDMI-A-1",
+  mode = "preferred",
+  scale = 1,
+  transform = 3,
+  position = "auto-center-left"
+})
+
+hl.monitor({
   output = "",
   mode = "preferred",
   position = "auto-center-up",
   scale = 1.0,
   transform = 0,
 })
-
-
-
