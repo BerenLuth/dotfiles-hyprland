e9f57e849d22678ea84933a63923efbe1f84d651
Fabio Rosada <r.fabio95@gmail.com>
2026-08-19 12:18:37 +0200

    cleaning up

---
+++ .config/hypr/execs.lua
@@ -0,0 +1,32 @@
+
+hl.on("hyprland.start", function ()
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("hypridle")
+  hl.exec_cmd("swaync")
+  hl.exec_cmd("hyprsunset")
+  hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("hyprpm reload")
+  hl.exec_cmd("openrgb --server && sleep 2 && openrgb -m static -c FF4400")
+  hl.exec_cmd('syshud -m "0 0 64 0"')
+  hl.exec_cmd("wal -i ~/.config/hypr/wallpaper/wallpaper && openrgb -c $(wal-accent)")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,ssh &")
+
+  hl.exec_cmd("sleep 2 && keepassxc &")
+  hl.exec_cmd("sleep 3 && nm-online --quiet --timeout 120 && megasync --minimized")
+
+  -- Disable bluetooth on startup
+  hl.exec_cmd("bluetoothctl list | grep -q \"Controller\" && bluetoothctl power off")
+
+  -- clipboard manager
+  hl.exec_cmd("wl-paste --type text --watch cliphist store # Stores only text data")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store # Stores only image data")
+
+  -- Hooks for power profiles management
+  hl.exec_cmd("~/.local/bin/power-profile-dbus-daemon.sh")
+
+  -- exec-once = bongocat -c ~/.config/bongocat/bongocat.conf &
+
+  -- Cursor
+  hl.exec_cmd("hyprctl setcursor Adwaita 24")
+end)
--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -11,43 +11,11 @@ local menu        = "~/.config/rofi/launchers/type-1/launcher.sh"
 local mainMonitor = "eDP-1"
 
 require("monitors")
-
+require("execs")
+local colors = dofile(os.getenv("HOME") .. "/.cache/wal/colors.lua")
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-
-
-hl.on("hyprland.start", function ()
-  hl.exec_cmd("waybar")
-  hl.exec_cmd("hypridle")
-  hl.exec_cmd("swaync")
-  hl.exec_cmd("hyprsunset")
-  hl.exec_cmd("hyprpaper")
-  hl.exec_cmd("hyprpm reload")
-  hl.exec_cmd("openrgb --server && sleep 2 && openrgb -m static -c FF4400")
-  hl.exec_cmd('syshud -m "0 0 64 0"')
-  hl.exec_cmd("wal -i ~/.config/hypr/wallpaper/wallpaper && openrgb -c $(wal-accent)")
-  hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,ssh &")
-
-  hl.exec_cmd("sleep 2 && keepassxc &")
-  hl.exec_cmd("sleep 3 && nm-online --quiet --timeout 120 && megasync --minimized")
-
-  -- Disable bluetooth on startup
-  hl.exec_cmd("bluetoothctl list | grep -q \"Controller\" && bluetoothctl power off")
-
-  -- clipboard manager
-  hl.exec_cmd("wl-paste --type text --watch cliphist store # Stores only text data")
-  hl.exec_cmd("wl-paste --type image --watch cliphist store # Stores only image data")
-
-  -- Hooks for power profiles management
-  hl.exec_cmd("~/.local/bin/power-profile-dbus-daemon.sh")
-
-  -- exec-once = bongocat -c ~/.config/bongocat/bongocat.conf &
-
-  -- Cursor
-  hl.exec_cmd("hyprctl setcursor Adwaita 24")
-end)
 
 hl.config({
   general = {
@@ -58,7 +26,7 @@ hl.config({
 
     col = {
       active_border = {
-        colors = {"rgba(33ccffee)", "rgba(00ff99ee)"},
+        colors = { colors.color1, colors.color10 },
         angle = 45,
       },
       inactive_border = "rgba(000000aa)"
@@ -214,6 +182,10 @@ hl.animation({
     bezier = "myBezier",
     style = "slidevert"
 })
+
+--------------
+-- GESTURES --
+--------------
 
 hl.gesture({ fingers = 3, direction = "vertical", action = "workspace" })
 hl.gesture({ fingers = 3, direction = "swipe", mods = "SUPER", action = "resize" })
