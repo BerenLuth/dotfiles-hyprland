756b38f45629e8911bfed9c43d4dfa23c89d3417
Fabio Rosada <r.fabio95@gmail.com>
2026-09-10 13:38:09 +0200

    new cursor

--- .config/hypr/execs.lua
+++ .config/hypr/execs.lua
@@ -30,3 +30,5 @@ hl.on("hyprland.start", function ()
   -- exec-once = bongocat -c ~/.config/bongocat/bongocat.conf &
 
   -- Cursor
+  hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 22")
+end)
